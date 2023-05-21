f747a138c46343346a11f45b8e54e9e6bf568b5d
milpen <milpen@mail.ru>
2023-05-21 21:29:29 +0500

    practice beginning

--- Practice/index.js
+++ Practice/index.js
@@ -1,15 +1,27 @@
 
-let BananaItem=document.getElementById('banana_price');
-let OrangeItem=document.getElementById('orange_price');
-let PineappleItem=document.getElementById('pineapple_price');
-let TomatoItem=document.getElementById('tomato_price');
-//console.log(BananaItem.innerHTML);
-//console.log(OrangeItem.innerHTML);
-//console.log(PineappleItem.innerHTML);
-//console.log(TomatoItem.innerHTML);
-//console.log(BananaItem.textContent);
-let NumberBananaItem=+BananaItem.textContent;
-//console.log(NumberBananaItem);
-let NumberOrangeItem=+OrangeItem.textContent;
-let NumberPineappleItem=+PineappleItem.textContent;
-let NumberTomatoItem=+TomatoItem.textContent;
+let bananaItem=document.getElementById('banana_price');
+let orangeItem=document.getElementById('orange_price');
+let pineappleItem=document.getElementById('pineapple_price');
+let tomatoItem=document.getElementById('tomato_price');
+//let NumberBananaItem=+BananaItem.textContent;
+//let NumberOrangeItem=+OrangeItem.textContent;
+//let NumberPineappleItem=+PineappleItem.textContent;
+//let NumberTomatoItem=+TomatoItem.textContent;
+const cartSum=+bananaItem.textContent + +orangeItem.textContent + +pineappleItem.textContent + +tomatoItem.textContent;
+const totalPrice=document.getElementById ('total_price');
+totalPrice.textContent=cartSum;
+//console.log(cartSum);
+
+
+function discount () {
+
+    totalPrice.textContent=(cartSum / 100 * 80);
+}
+
+let discountBananaItem=Number(bananaItem.textContent);
+
+function discountBanana () {
+    discountBananaItem.textContent=(discountBananaItem / 100 * 80);
+}
+const button= document.getElementById("btn");
+button.addEventListener("click", discount, discountBanana);
