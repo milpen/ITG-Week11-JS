0f62b0f71d525c92f3cd145e8180ccae65b17128
milpen <milpen@mail.ru>
2023-05-21 22:04:25 +0500

    js works

--- Practice/index.js
+++ Practice/index.js
@@ -3,25 +3,26 @@ let bananaItem=document.getElementById('banana_price');
 let orangeItem=document.getElementById('orange_price');
 let pineappleItem=document.getElementById('pineapple_price');
 let tomatoItem=document.getElementById('tomato_price');
-//let NumberBananaItem=+BananaItem.textContent;
-//let NumberOrangeItem=+OrangeItem.textContent;
-//let NumberPineappleItem=+PineappleItem.textContent;
-//let NumberTomatoItem=+TomatoItem.textContent;
 const cartSum=+bananaItem.textContent + +orangeItem.textContent + +pineappleItem.textContent + +tomatoItem.textContent;
 const totalPrice=document.getElementById ('total_price');
 totalPrice.textContent=cartSum;
-//console.log(cartSum);
 
-
-function discount () {
+/*function discount () {
 
     totalPrice.textContent=(cartSum / 100 * 80);
-}
+}*/
 
-let discountBananaItem=Number(bananaItem.textContent);
+let discountBananaItem=(Number(bananaItem.textContent)) / 100 * 80;
+let discountOrangeItem=(Number(orangeItem.textContent)) / 100 * 80;
+let discountPineappleItem=(Number(pineappleItem.textContent)) / 100 * 80;
+let discountTomatoItem=(Number(tomatoItem.textContent)) / 100 *80;
 
-function discountBanana () {
-    discountBananaItem.textContent=(discountBananaItem / 100 * 80);
+function discount () {
+    bananaItem.textContent=discountBananaItem;
+    orangeItem.textContent=discountOrangeItem;
+    pineappleItem.textContent=discountPineappleItem;
+    tomatoItem.textContent=discountTomatoItem;
+    totalPrice.textContent= discountBananaItem + discountOrangeItem + discountPineappleItem + discountTomatoItem;
 }
 const button= document.getElementById("btn");
-button.addEventListener("click", discount, discountBanana);
+button.addEventListener("click", discount);
