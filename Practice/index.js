a8bc023e487267a01c9c8169f0160137a9899987
milpen <milpen@mail.ru>
2023-05-21 20:01:22 +0500

    soft skills is ready

---
+++ Practice/index.js
@@ -0,0 +1,15 @@
+
+let BananaItem=document.getElementById('banana_price');
+let OrangeItem=document.getElementById('orange_price');
+let PineappleItem=document.getElementById('pineapple_price');
+let TomatoItem=document.getElementById('tomato_price');
+//console.log(BananaItem.innerHTML);
+//console.log(OrangeItem.innerHTML);
+//console.log(PineappleItem.innerHTML);
+//console.log(TomatoItem.innerHTML);
+//console.log(BananaItem.textContent);
+let NumberBananaItem=+BananaItem.textContent;
+//console.log(NumberBananaItem);
+let NumberOrangeItem=+OrangeItem.textContent;
+let NumberPineappleItem=+PineappleItem.textContent;
+let NumberTomatoItem=+TomatoItem.textContent;
